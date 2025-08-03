3abd1e0d4ba768cf279e79d0a36c7292cf9fae1b
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-03 14:08:27 -0400

    frontmatter support

--- site/build-scripts/build-tutorials.js
+++ site/build-scripts/build-tutorials.js
@@ -2,12 +2,20 @@ import { renderMarkdown } from "./render-markdown.js";
 import { writePage } from "./write-page.js";
 import { resolve, parse } from "node:path";
 import { readdir, readFile, mkdir, copyFile } from "node:fs/promises";
+import matter from "gray-matter";
 
 async function buildTutorial(dir, name) {
   const filePath = resolve(`site/tutorials/${dir}/${name}.md`);
   const source = await readFile(filePath, "utf8");
-  const main = await renderMarkdown(filePath, source);
-  await writePage(`tutorials/${dir}/${name}.html`, name, "base.css", "", main);
+  const { data, content } = matter(source);
+  const main = await renderMarkdown(filePath, content);
+  await writePage(
+    `tutorials/${dir}/${name}.html`,
+    data.title,
+    "base.css",
+    "",
+    main,
+  );
 }
 
 export async function buildTutorials() {
