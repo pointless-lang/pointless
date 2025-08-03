9bcb1817a5362c48eeaed27af8a9fde71ae60d57
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-03 15:21:54 -0400

    add symlink options build flag for faster css prototyping

--- site/build.js
+++ site/build.js
@@ -1,12 +1,22 @@
 import { buildIcons } from "./build-scripts/build-icons.js";
 import { buildStd } from "./build-scripts/build-std.js";
 import { buildTutorials } from "./build-scripts/build-tutorials.js";
-import { cp, rm, mkdir } from "node:fs/promises";
+import { readdir, symlink, cp, rm, mkdir } from "node:fs/promises";
+import args from "command-line-args";
+
+const options = args([{ name: "prod", type: Boolean, defaultValue: false }]);
 
 async function init() {
   await rm("site/dist", { recursive: true, force: true });
   await mkdir("site/dist", { recursive: true });
-  await cp("site/static", "site/dist", { recursive: true });
+
+  if (options.prod) {
+    await cp("site/static", "site/dist", { recursive: true });
+  } else {
+    for (const name of await readdir("site/static")) {
+      await symlink(`../static/${name}`, `site/dist/${name}`);
+    }
+  }
 }
 
 await init();
