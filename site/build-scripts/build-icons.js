cf61c3897698378474e62adfa8c245c7f46ac835
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-04 17:50:00 -0400

    make svgs available for site

--- site/build-scripts/build-icons.js
+++ site/build-scripts/build-icons.js
@@ -3,13 +3,11 @@ import path from "node:path";
 import sharp from "sharp";
 
 export async function buildIcons() {
-  await mkdir(`site/dist/icons`, { recursive: true });
-
-  for (const file of await readdir("site/icons")) {
+  for (const file of await readdir("site/dist/icons")) {
     const name = path.basename(file, ".svg");
 
-    await sharp(await readFile(`site/icons/${name}.svg`))
+    await sharp(await readFile(`site/dist/icons/${name}.svg`))
       .png()
-      .toFile(`site/dist/icons/${name}.png`);
+      .toFile(`site/dist/${name}.png`);
   }
 }
