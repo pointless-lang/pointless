e250c4e7e9001504fb37e11ae1e63f58b0a8ed6d
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-11 14:16:29 -0400

    move ptls.js, add demo notebook feature

--- ptls.js
+++ ptls.js
@@ -1,11 +1,14 @@
+import { getImport } from "./src/import.js";
+import { Panic } from "./src/panic.js";
+import { repl } from "./repl/repl.js";
+import { serve } from "./notebook/serve.js";
 import { argv } from "node:process";
-import { getImport } from "./import.js";
-import { Panic } from "./panic.js";
-import { repl } from "./repl.js";
 
 async function run() {
   try {
-    if (argv.length > 2) {
+    if (argv.length > 3) {
+      await serve(argv[3]);
+     } else if (argv.length > 2) {
       await getImport(argv[2], "./");
     } else {
       await repl();
