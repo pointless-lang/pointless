7129c2eba0c7da1a878b983f484f651e0bedb104
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-15 20:07:45 -0400

    remove source path accessor

--- std/lang/mod.js
+++ std/lang/mod.js
@@ -1,13 +1,4 @@
 import { spawnStd } from "../std.js";
-import { basename, dirname } from "node:path";
-import { OrderedMap } from "immutable";
-
-export function srcPath() {
-  return OrderedMap({
-    name: basename(Env.currentPath),
-    path: dirname(Env.currentPath),
-  });
-}
 
 export async function getPrelude() {
   return OrderedMap((await spawnStd()).parent.defs);
