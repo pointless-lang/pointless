a06b2fd168482d9df3af381caec89e5ce19ee5e7
avery nortonsmith <averynortonsmith@fastmail.com>
2025-08-18 16:53:04 -0400

    simplify escape code

--- site/build-scripts/escape.js
+++ site/build-scripts/escape.js
@@ -10,18 +10,18 @@ function escapeHtml(string) {
 export function h(strings, ...values) {
   let result = "";
 
-  for (let i = 0; i < values.length; i++) {
-    const str = strings[i];
+  for (const [index, value] of values.entries()) {
+    const str = strings[index];
 
     if (str.endsWith("$$")) {
-      const val = [values[i]]
+      const val = [value]
         .flat(Infinity)
         .map((val) => (val ? val : ""))
         .join("");
 
       result += str.slice(0, -2) + val;
     } else {
-      const val = [values[i]].flat(Infinity).map(String).join("");
+      const val = [value].flat(Infinity).map(String).join("");
       result += str + escapeHtml(val);
     }
   }
