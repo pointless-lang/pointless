115bcb3e38743058ac96353965c6fbfd9d715dde
avery nortonsmith <averynortonsmith@fastmail.com>
2025-12-23 23:23:11 -0500

    indexOf param order

--- std/List.js
+++ std/List.js
@@ -400,13 +400,13 @@ export async function find(list, condition) {
   return null;
 }
 
-export function indexOf(value, list) {
+export function indexOf(list, value) {
   // Get the index of the first occurence of `value` in `list`, or `none` if
   // `value` does not appear in `list`.
   //
   // ```ptls
-  // List.indexOf(9, [1, 9, 9, 6])
-  // List.indexOf(0, [1, 9, 9, 6])
+  // List.indexOf([1, 9, 9, 6], 9)
+  // List.indexOf([1, 9, 9, 6], 0)
   // ```
 
   checkType(list, "list");
