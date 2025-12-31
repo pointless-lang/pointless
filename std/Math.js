d7ad9cba9f662ad53993fd912dbfb8d4ee18130d
avery nortonsmith <averynortonsmith@fastmail.com>
2025-12-31 00:14:18 -0500

    refactor rounding code (should be equivalent?)

--- std/Math.js
+++ std/Math.js
@@ -87,11 +87,6 @@ export function roundTo(n, decimals) {
   checkType(n, "number");
   checkWhole(decimals);
 
-  if (decimals < 0) {
-    const factor = 10 ** -decimals;
-    return round(n / factor) * factor;
-  }
-
   const factor = 10 ** decimals;
   return round(n * factor) / factor;
 }
